00430f4028376f2a078aaa5ed0bfc98e3a2dd554
qanitasyaf <qanitafiqa@gmail.com>
2025-07-13 21:46:12 +0700

    test

--- login-system/login/src/test/java/com/example/login/LoginApplicationTests.java
+++ login-system/login/src/test/java/com/example/login/LoginApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.login;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LoginApplicationTests {
 
 	@Test
